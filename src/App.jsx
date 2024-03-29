aba425042da97e0eab3600c3cfd410394a3655fd
Abdi Kumela <abdikumela036@gmail.com>
2024-03-29 12:16:37 +0300

    finsished styling UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Product from "./Pages/Product";
 import Pricing from "./Pages/Pricing";
 import HomePage from "./Pages/HomePage";
 import PageNotFound from "./Pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -11,6 +14,8 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
